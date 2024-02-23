0c63f3ccb2c54886adf85e64615b840b6f8e3476
Kishore.s <Kishore.s@KISHORES-SDT>
2024-02-23 19:40:46 +0530

    Pugin base service url modified.

--- src/app/services/plugin-services/plugin-base.service.ts
+++ src/app/services/plugin-services/plugin-base.service.ts
@@ -17,32 +17,32 @@ export class PluginService {
   }
 
   getAll(): Observable<Plugin[]> {
-    return this._http.get<Plugin[]>("Plugin/get_all");
+    return this._http.get<Plugin[]>("Plugin/plugins/all");
   }
 
   add(data: Plugin): Observable<any> {
-    return this._http.post("Plugin/add_plugin", data, {
+    return this._http.post("Plugin/plugin/post", data, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     })
   }
 
   update(id: number, data: Plugin): Observable<any> {
-    return this._http.put("Plugin/update_plugin/" + id, data, {
+    return this._http.put("Plugin/plugin/put/" + id, data, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     })
   }
 
   remove(id: number): Observable<any> {
-    return this._http.delete("Plugin/remove/" + id);
+    return this._http.delete("Plugin/plugin/delete/" + id);
   }
 
   getByDepartment(): Observable<any> {
     // console.log(this._lsService.getDepartmentId());
-    return this._http.get<Plugin[]>(`Plugin/get_by_department/departmentid=${this._lsService.getDepartmentId()}`);
+    return this._http.get<Plugin[]>(`Plugin/departments/${this._lsService.getDepartmentId()}`);
   }
 
   getWithLog(pluginId: number): Observable<Plugin> {
-    return this._http.get<Plugin>(`Plugin/get_withlog/pluginId=${pluginId}`);
+    return this._http.get<Plugin>(`Plugin/plugins/${pluginId}/log`);
   }
 
   getWithLogByMonthAndYear(pluginId: number, month: number, year: number): Observable<Plugin> {
@@ -50,7 +50,7 @@ export class PluginService {
     // Plugin/get_withlog_by_monthandyear/departmentid=1&month=2&year=2024
     // [HttpGet("get_withlog_by_monthyear/pluginId={pluginId}&month={month}&year={year}")]
     // [HttpGet("get_withlog_by_monthyear/pluginId={pluginId}&month={month}&year={year}")]
-    return this._http.get<Plugin>(`Plugin/get_withlog_by_monthyear/pluginId=${pluginId}&month=${month}&year=${year}`);
+    return this._http.get<Plugin>(`Plugin/plugin/${pluginId}/log/${month}/${year}`);
   }
 
   getAllWithLogByMonthAndYear(month: number, year: number): Observable<Plugin[]> {
@@ -58,10 +58,10 @@ export class PluginService {
     // Plugin/get_withlog_by_monthandyear/departmentid=1&month=2&year=2024
     // [HttpGet("get_withlog_by_monthyear/pluginId={pluginId}&month={month}&year={year}")]
 
-    return this._http.get<Plugin[]>(`Plugin/get_all_withlog_by_monthandyear/departmentid=${this._lsService.getDepartmentId()}&month=${month}&year=${year}`);
+    return this._http.get<Plugin[]>(`Plugin/plugins/${this._lsService.getDepartmentId()}/log/${month}/${year}`);
   }
 
   getWithLogByYear(pluginId:number, year: number): Observable<Plugin> {
-    return this._http.get<Plugin>(`Plugin/get_withlog_by_year/pluginId=${pluginId}&year=${year}`);
+    return this._http.get<Plugin>(`Plugin/plugin/${pluginId}/log/${year}`);
   }
 }
